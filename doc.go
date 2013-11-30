5940a78376eb1c659d60e3de58b9026d651f55c8
Kyle Isom <kyle@tyrfingr.is>
2013-11-29 17:35:03 -0700

    Add documentation.

---
+++ doc.go
@@ -0,0 +1,24 @@
+/*
+   package catena implements the catena memory-consuming password
+   scrambler.
+
+   The package currently supports the basic scrambling functions,
+   with a focus on password hashing. Further releases will see more
+   support for the extended parts of Catena, including key derivation,
+   server relief, and client-independent updates.
+
+   The tweak can be nil, but it represents an ability to inject
+   additional information (include the purpose, hash size, and salt
+   length) in the final output.
+
+   The garlic is a parameter that controls the amount of memory
+   used.  This implementation restricts the garlic to at most 31.
+   The documentation contains performance notes that may be of
+   interest to the user. The Catena paper recommends setting the
+   initial garlic value to the actual garlic value as a balance
+   between memory required and performance.
+
+   The paper recommends a salt length of 16 bytes, but this parameter
+   is left up to the user to determine.
+*/
+package catena
